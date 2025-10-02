72c730d0bfeab2afe8d32be828c0cbb96c7c5bda
amany <amanyabdelnaby1569@gmail.com>
2025-10-02 19:07:18 +0300

    added database script

--- Demo/obj/Debug/net9.0/Demo.AssemblyInfo.cs
+++ Demo/obj/Debug/net9.0/Demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6b90a1fc8df0891edc96d19c5a32652515d59bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
